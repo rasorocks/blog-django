150c8b1ce0c33fafe8c1ac5d059dd7a4005899da
Rafael Raso <rasorocks@gmail.com>
2025-03-24 12:42:00 -0300

    Added dockecompose

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -24,3 +24,9 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
